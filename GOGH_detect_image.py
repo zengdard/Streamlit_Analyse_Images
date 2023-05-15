60da4db098d9777727034b3ee7795d17ddfcf36f
Nielzaclord <61885397+Nielzaclord@users.noreply.github.com>
2023-05-15 17:11:12 +0200

    Update GOGH_detect_image.py

--- GOGH_detect_image.py
+++ GOGH_detect_image.py
@@ -7,8 +7,8 @@ from st_on_hover_tabs import on_hover_tabs
 st.title("StendhalGPT Gogh")
 
 with st.sidebar:
-        tabs = on_hover_tabs(tabName=['Accueil','Example'], 
-                            iconName=['dashboard','home'],
+        tabs = on_hover_tabs(tabName=['Accueil'], 
+                            iconName=['dashboard'],
                             styles = {'navtab': {'background-color':'#FFFFFF',
                                                 'color': '#000000',
                                                 'font-size': '18px',
@@ -29,84 +29,52 @@ with st.sidebar:
 
 col5, col6 = st.columns(2)
 
-if tabs == 'Accueil':
-        
-        st.info('Nous prévoyons d\'intégrer prochainement un modèle basé sur YOLOv5, afin d\'améliorer les capacités de détection.')
 
-        uploaded_file = st.file_uploader("Choose an image...", type=["jpg","png","jpeg"])
-        if uploaded_file is not None:
-            image = Image.open(uploaded_file)
-            st.image(image, caption='Uploaded Image.', use_column_width=True)
-            st.write("")
+st.info('Nous prévoyons d\'intégrer prochainement un modèle basé sur YOLOv5, afin d\'améliorer les capacités de détection.')
 
-            # Convertir l'image en un tableau numpy pour le traitement
-            image = np.array(image)
-            image_ela = image.copy()  # Copie de l'image pour l'appliquer sur le filtre ELA
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg","png","jpeg"])
+if uploaded_file is not None:
+image = Image.open(uploaded_file)
+st.image(image, caption='Uploaded Image.', use_column_width=True)
+st.write("")
 
-            # Convertir l'image en niveaux de gris
-            grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Convertir l'image en un tableau numpy pour le traitement
+image = np.array(image)
+image_ela = image.copy()  # Copie de l'image pour l'appliquer sur le filtre ELA
 
-            # Appliquer le filtre ELA avec le filtre de différence absolue comme noyau
-            ela = cv2.absdiff(grayscale, cv2.blur(grayscale, (5, 5)))
+# Convertir l'image en niveaux de gris
+grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-            # Seuiller l'image ELA pour obtenir une image binaire
-            threshold_value = 10.5
-            threshold = cv2.threshold(ela, threshold_value, 255, cv2.THRESH_BINARY)[1]
+# Appliquer le filtre ELA avec le filtre de différence absolue comme noyau
+ela = cv2.absdiff(grayscale, cv2.blur(grayscale, (5, 5)))
 
-            # Effectuer une analyse de connectivité pour identifier les groupes de pixels connectés
-            connectivity = 8
-            output = cv2.connectedComponentsWithStats(threshold, connectivity, cv2.CV_32S)
+# Seuiller l'image ELA pour obtenir une image binaire
+threshold_value = 10.5
+threshold = cv2.threshold(ela, threshold_value, 255, cv2.THRESH_BINARY)[1]
 
-            # Récupérer les informations sur les composantes connectées
-            num_labels = output[0]
-            labels = output[1]
-            stats = output[2]
+# Effectuer une analyse de connectivité pour identifier les groupes de pixels connectés
+connectivity = 8
+output = cv2.connectedComponentsWithStats(threshold, connectivity, cv2.CV_32S)
 
-            # Parcourir les composantes connectées
-            for label in range(1, num_labels):
-                # Récupérer les coordonnées du rectangle englobant
-                x, y, w, h = stats[label, cv2.CC_STAT_LEFT], stats[label, cv2.CC_STAT_TOP], stats[label, cv2.CC_STAT_WIDTH], stats[label, cv2.CC_STAT_HEIGHT]
-                # Dessiner un carré autour du groupe de pixels
+# Récupérer les informations sur les composantes connectées
+num_labels = output[0]
+labels = output[1]
+stats = output[2]
 
-                if w*h > 280:
-                    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                    cv2.rectangle(grayscale, (x, y), (x+w, y+h), (0, 255, 0), 2)
+# Parcourir les composantes connectées
+for label in range(1, num_labels):
+# Récupérer les coordonnées du rectangle englobant
+        x, y, w, h = stats[label, cv2.CC_STAT_LEFT], stats[label, cv2.CC_STAT_TOP], stats[label, cv2.CC_STAT_WIDTH], stats[label, cv2.CC_STAT_HEIGHT]
+# Dessiner un carré autour du groupe de pixels
 
-            # Convertir les tableaux numpy en images PIL pour l'affichage
-            image = Image.fromarray(image)
-            image_ela = Image.fromarray(grayscale)
+        if w*h > 280:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.rectangle(grayscale, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-            # Afficher l'image avec les carrés dessinés
-            st.image(image, caption='Image with detected regions.', use_column_width=True)
-            st.image(ela, caption='ELA image with detected regions.', use_column_width=True)
-elif tabs == 'Example' :
-    with col5:
-        st.markdown('## Problèmes typiques d\'une génération :')
-        st.markdown("""
-    <ul>
-        <li>Éléments notables distordus ou faux (Drapeaux, logos, éléments distinctifs, etc)</li>
-        <li>Netteté ou incohérence d'éléments précis (Visages, mains, insignes) </li>
-        <li>Ombres mals représentées</li>
-    </ul>
-    """, unsafe_allow_html=True)
-        
+# Convertir les tableaux numpy en images PIL pour l'affichage
+image = Image.fromarray(image)
+image_ela = Image.fromarray(grayscale)
 
-        image = Image.open('donald_trump_marche_fake.png')
-        st.image(image, caption='Image Générée')
-        image = Image.open('pape_fake_2.jpg')
-        st.image(image, caption='Image Générée')
-    with col6:
-        
-        st.markdown('## Éléments à prendre en compte :')
-        st.markdown("""
-    <ul>
-        <li>Qualité et netteté générale de la photo</li>
-    </ul>
-    """, unsafe_allow_html=True)
-        image = Image.open('pape_true.jpg')
-        st.image(image, caption='Image Non Générée')
-
-        image = Image.open('fake_generate.jpg')
-        st.image(image, caption='Image Non Générée')
-        image = Image.open('fake_generate_2.jpg')
-        st.image(image, caption='Image Non Générée')
+# Afficher l'image avec les carrés dessinés
+st.image(image, caption='Image with detected regions.', use_column_width=True)
+st.image(ela, caption='ELA image with detected regions.', use_column_width=True)
